dd50248b66ec89edb2e5fc05dc7063ef5c43fda7
Muhammet <muhammetvural@anadolu.edu.tr>
2023-03-27 14:42:34 +0300

    colors

---
+++ lib/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+ 
+  MyApp._internal();// private named constractor
+
+  static final MyApp instance = MyApp._internal(); // single instance ya da singleton
+
+  int appState = 0;
+
+  factory MyApp() => instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
